a64a0ac0802935ec17920fbbe408392516716b5d
Ann Serdukova <404region@gmail.com>
2022-09-01 12:10:33 +0300

    Код после npm run build

--- js/toggle-btn.js
+++
@@ -1,9 +0,0 @@
-let toggleBtn = document.getElementById('read-more-toggle-btn');
-let toggleBlock = document.getElementById('toggle-text-block');
-let toggleBlockOpen = true;
-
-
-toggleBtn.addEventListener('click', () => {
-    toggleBlock.style.maxHeight = (toggleBlockOpen) ? 'initial' : '90px';
-    toggleBlockOpen = !toggleBlockOpen;
-});
